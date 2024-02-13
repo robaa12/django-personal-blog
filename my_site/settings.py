9e796935547ed0cef61d1c181b53ad9ef11cdc94
Robaa <arobaa23@gmail.com>
2024-02-14 00:00:47 +0200

    Final Version For Deplyment

--- my_site/settings.py
+++ my_site/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
